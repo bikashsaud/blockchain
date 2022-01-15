a7fe425378535be0a1b6c71808db547c02710eed
Bikash Saud <saudbikash514@gmail.com>
2022-01-15 22:43:23 +0545

    transection oncept

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,17 @@
-blockchain = [1]
+blockchain = []
 
-def add_value(val):
-    blockchain.append([blockchain[0],val])
-    print(blockchain)
 
-add_value(12)
-add_value(14)
-add_value(19)
-add_value(43)
+def get_last_blockchain_value():
+    return blockchain[-1]
+    
+
+def add_value(transation_amount, last_transection):
+    blockchain.append([last_transection, transation_amount])
+    
+    
+
+add_value(12, [1])
+add_value(0.1, get_last_blockchain_value())
+add_value(19, get_last_blockchain_value())
+add_value(43, get_last_blockchain_value())
+print (blockchain)
